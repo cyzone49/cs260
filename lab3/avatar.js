1e20e628e0d5a6df2fe1c60248605a04dd73bf1b
cyzone49 <datvu4749@gmail.com>
2018-10-10 22:44:29 +0000

    finished structure of files

---
+++ lab3/avatar.js
@@ -0,0 +1,49 @@
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', avatarDirective );
+
+function mainCtrl ($scope) {
+    $scope.users = [];
+    
+    $scope.addNew = function (user) {
+        
+        if ( user.name === undefined && user.email === undefined ) {
+            alert("Cannot add without both Name and Email");
+            return;
+        } else {
+            $scope.users.push({
+                name: user.name,
+                email: user.email,
+                avatarUrl: user.url
+            })    
+        }
+        user.name = "";
+        user.email = "";
+        user.url = "";
+    };
+}
+
+ 
+function avatarDirective() {
+    return {
+        scope: {
+         user: '='
+        },
+        restrict: 'E',
+        replace: 'true',
+        template: (
+            '<div class="Avatar">' +
+                '<img ng-src="{{user.avatarUrl}}" />' +
+                '<h4>Name: {{user.name}}</h4>' +
+                '<h4>Email: {{user.email}}</h4>' +
+            '</div>'   
+        ),
+        link: link
+    };
+    
+    function link(scope) {
+        if (!scope.user.avatarUrl) {
+            scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
+        }
+    }
+}
